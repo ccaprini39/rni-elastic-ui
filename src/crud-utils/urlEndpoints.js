0b936aa32880416cfa65267e5a5c90e0a8d5b50b
Clouis <ccaprini39@gmail.com>
2022-10-04 08:24:55 -0400

    adding correct variable

--- src/crud-utils/urlEndpoints.js
+++ src/crud-utils/urlEndpoints.js
@@ -1,9 +1,9 @@
 export const ELASTICSEARCH_URL = 'http://ec2-18-222-127-24.us-east-2.compute.amazonaws.com:9200/'
 
 
-//export const SPRING_BOOT_APP_URL = 'http://localhost:8080'
+export const SPRING_BOOT_APP_URL = 'http://localhost:8080'
 //export const SPRING_BOOT_APP_URL = 'http://ec2-18-222-127-24.us-east-2.compute.amazonaws.com:8080' //original
-export const SPRING_BOOT_APP_URL = 'http://ec2-18-220-93-214.us-east-2.compute.amazonaws.com:8080' //flat
+//export const SPRING_BOOT_APP_URL = 'http://ec2-18-220-93-214.us-east-2.compute.amazonaws.com:8080' //flat
 //export const SPRING_BOOT_APP_URL = 'http://ec2-18-217-23-131.us-east-2.compute.amazonaws.com:8080' //nested
 
 
